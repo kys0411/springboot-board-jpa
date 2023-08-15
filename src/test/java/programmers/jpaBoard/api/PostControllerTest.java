643c9035080b8f9630c537a045a87db1fadf3c1d
kys0411 <32180846@dankook.ac.kr>
2023-08-15 12:49:33 +0900

    ✅ restdocs를 위한 테스트 코드 추가

--- src/test/java/programmers/jpaBoard/api/PostControllerTest.java
+++ src/test/java/programmers/jpaBoard/api/PostControllerTest.java
@@ -4,10 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import programmers.jpaBoard.dto.CreatedPost;
 import programmers.jpaBoard.dto.PostResponse;
@@ -15,14 +20,22 @@ import programmers.jpaBoard.dto.UpdatedPost;
 import programmers.jpaBoard.repository.PostRepository;
 import programmers.jpaBoard.service.PostService;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@ExtendWith({RestDocumentationExtension.class})
 class PostControllerTest {
 
     @Autowired
@@ -54,10 +67,21 @@ class PostControllerTest {
 
     @Test
     void getOne() throws Exception {
-        mockMvc.perform(get("/posts/{id}", response.id())
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/posts/{id}", response.id())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("post-get-one",
+                        pathParameters(
+                                parameterWithName("id").description("Post Id")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("Post Id"),
+                                fieldWithPath("title").description("title"),
+                                fieldWithPath("content").description("content"),
+                                fieldWithPath("createdAt").description("createdAt")
+                        )
+                ));
     }
 
     @Test
@@ -67,8 +91,13 @@ class PostControllerTest {
                         .param("size", String.valueOf(10))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(print());
-
+                .andDo(print())
+                .andDo(document("post-get-all",
+                        queryParameters(
+                                parameterWithName("page").description("The page to retrieve"),
+                                parameterWithName("size").description("Entries per page")
+                        )
+                ));
     }
 
     @Test
@@ -79,17 +108,38 @@ class PostControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("post-create",
+                        requestFields(
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("title"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("content")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("Post Id"),
+                                fieldWithPath("title").description("title"),
+                                fieldWithPath("content").description("content"),
+                                fieldWithPath("createdAt").description("createdAt")
+                        )
+                ));
     }
 
     @Test
     void update() throws Exception {
         UpdatedPost.Request request = new UpdatedPost.Request("update", "updateContent");
 
-        mockMvc.perform(put("/posts/{id}", response.id())
+        mockMvc.perform(RestDocumentationRequestBuilders.put("/posts/{id}", response.id())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("post-update",
+                        pathParameters(
+                                parameterWithName("id").description("Post Id")
+                        ),
+                        requestFields(
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("title"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("content")
+                        )
+                ));
     }
 }
